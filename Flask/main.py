82ed8d48c61a44a236d1ce0572eaca5a7b99191b
kdyvik <klaus.dyvik@gmail.com>
2019-02-05 12:33:24 +0100

    subfolder

--- Flask/main.py
+++ Flask/main.py
@@ -5,24 +5,24 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/kjop')
 def kjop():
-    return render_template('kjop.html')
+    return render_template('templates/kjop.html')
 
 @app.route('/salg')
 def salg():
-    return render_template('salg.html')
+    return render_template('templates/salg.html')
 
 @app.route('/historikk')
 def historikk():
-    return render_template('historikk.html')
+    return render_template('templates/historikk.html')
 
 @app.route('/profil')
 def profil():
-    return render_template('profil.html')
+    return render_template('templates/profil.html')
 
 @app.route('/logg_inn')
 def logg_inn():
-    return render_template('logg_inn.html')
+    return render_template('templates/logg_inn.html')
 
 @app.route('/glemt_passord')
 def glemt_passord():
-    return render_template('glemt_passord.html')
+    return render_template('templates/glemt_passord.html')

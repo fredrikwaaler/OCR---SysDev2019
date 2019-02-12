b81c2be7e9e16f587188c08c624ea5789496fa5b
Mordi490 <jananton007@hotmail.com>
2019-02-12 11:07:55 +0100

    kontrakt --> kontakt

--- Flask/forms.py
+++ Flask/forms.py
@@ -31,7 +31,7 @@ class SalgForm(FlaskForm):
     type_salg = SelectField('Type', choices=[])
     kunde = SelectField('Kunde', choices=[])
     dato = StringField('Dato')
-    kontrakt = SelectField('Kontrakt', choices=[])
+    kontakt = SelectField('kontakt', choices=[])
     kommentar = StringField('Kommentar')
     din_referanse = StringField('Din Referanse')
     kontonummer = StringField('Kontonummer')

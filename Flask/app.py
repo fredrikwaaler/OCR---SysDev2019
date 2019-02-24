7c89975427d342e1bac4e4ee77d2b66d9a1376ec
kdyvik <klaus.dyvik@gmail.com>
2019-02-24 19:11:59 +0100

    Added upload functionality. Stores uploaded file in uploads

--- Flask/app.py
+++ Flask/app.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, render_template, flash, request, redirect, url_for
 from flask_wtf import FlaskForm
 from forms import LoginForm, ForgotForm, KjoopForm, SalgForm
 from flask_nav import Nav
 from flask_nav.elements import Navbar, View
+from werkzeug.utils import secure_filename
+
+
 
 app = Flask(__name__)
+#Navbar
 nav = Nav(app)
 app.config['SECRET_KEY'] = 'secretkey'
 
@@ -18,10 +23,14 @@ nav.register_element('nav', navbar)
 
 nav.init_app(app)
 
+#Uploading files to server
+UPLOAD_FOLDER = 'uploads'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
 
 @app.route('/')
-
-
 @app.route('/kjoop', methods=['GET', 'POST'])
 def kjoop():
     form = KjoopForm()
@@ -70,5 +79,30 @@ def glemt_passord():
         return "Forgot-form validated"
     return render_template('glemt_passord.html', title="Glemt Passord", form=form)
 
+
+@app.route('/upload_file', methods=['POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return "success"
+    return "None"
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+    
 if __name__ == '__main__':
     app.run(debug=True)

0d7127fc0947fee48f6858ce566134d6358c5314
Yrianhovde <37143550+Yrianhovde@users.noreply.github.com>
2019-03-07 12:23:49 +0100

    Revert "Revert "gets access to google vision api, can send images that are locally""
    This reverts commit a0b4d1547ed4f7886aa4a175277497f5e6480409.

---
+++ Python/GetAccess.py
@@ -0,0 +1,42 @@
+import requests
+import json
+import base64
+from google.oauth2 import service_account
+from google.cloud import vision
+
+class VisionAPI:
+    __author__ = "Yrian Hovde Øksne"
+    """
+    Made for interacting with the Google Vision API
+    """
+    key = "3b6baaea984e1382d7528456c19633bbdd949e31"
+
+
+    @staticmethod
+    def get_text_detection_from_img(image_name):
+        """
+        gets a json object containing text detection analysis information from the google vision api
+        :param img_name: name of the image you want to analyze, needs .fileType annotation, image has to be in the same
+        folder as the executed file
+        :return: json object with text detection analysis details
+        """
+        '''
+        establishes connection with the api
+        '''
+        credentials = service_account.Credentials.from_service_account_file("key.json")
+        client = vision.ImageAnnotatorClient(credentials=credentials)
+        '''
+        Finds the image and converts it to a base64 encoded string
+        '''
+        with open(image_name, "rb") as image:
+            content = image.read()
+        '''
+        gets and prints response from the api
+        '''
+        response = client.annotate_image(
+            {"image": {"content": content}, "features":
+                [{'type': vision.enums.Feature.Type.TEXT_DETECTION}]})
+        print(response)
+
+
+a = VisionAPI.get_text_detection_from_img("kvit.jpg")

357b41799e83b6d062d85a37c72543a6702ee134
Yrianhovde <37143550+Yrianhovde@users.noreply.github.com>
2019-03-19 11:32:01 +0100

    refactored version. Extended and updated documentation.

--- Python/ImageProcessor.py
+++ Python/ImageProcessor.py
@@ -1,34 +1,31 @@
-import requests
-import json
-import base64
 from google.oauth2 import service_account
 from google.cloud import vision
 from datetime import datetime
 import re
-import json
 
 
-class VisionAPI:
+class VisionManager:
     __author__ = "Yrian Hovde Øksne"
-    """
-    Made for interacting with the Google Vision API
-    """
-    key = "3b6baaea984e1382d7528456c19633bbdd949e31"
-
-
-    @staticmethod
-    def get_text_detection_from_img(image_name):
+    def __init__(self, gvision_auth_key_path):
         """
-        gets a json object containing text detection analysis information from the google vision api
-        :param img_name: name of the image you want to analyze, needs .fileType annotation, image has to be in the same
-        folder as the executed file
-        :return: json object with text detection analysis details
+        The class VisionManager serves to interact with Googles Vision API. The class needs a path for the google
+        vision authentication key to be able to interact with the api. The key needs to be in a json file.
+        Note: class is used in syst.dev. subject to satisfy the owner of @Sukkertoppen's requirements.
+        :param gvision_auth_key_path: path of the google vision authentication key. This needs to be a json file.
         """
         '''
         establishes connection with the api
         '''
-        credentials = service_account.Credentials.from_service_account_file("key.json")
-        client = vision.ImageAnnotatorClient(credentials=credentials)
+        self._credentials = service_account.Credentials.from_service_account_file(gvision_auth_key_path)
+        self._client = vision.ImageAnnotatorClient(credentials=self._credentials)
+
+    def get_text_detection_from_img(self, image_name):
+        """
+        gets a json object containing text detection analysis information from the google vision api
+        :param img_name: name of the image you want to analyze, needs .fileType annotation, image has to be in the same
+        folder as the executed file
+        :return: plain text of the text within the image given as parameter
+        """
         '''
         Finds the image and converts it to a base64 encoded string
         '''
@@ -37,16 +34,8 @@ class VisionAPI:
         '''
         gets and prints response from the api
         '''
-
-        '''
-        response = client.annotate_image(
-            {"image": {"content": content}, "features":
-                [{'type': vision.enums.Feature.Type.TEXT_DETECTION}]})
-        '''
-
         image = vision.types.Image(content=content)
-
-        response = client.text_detection(image=image)
+        response = self._client.text_detection(image=image)
         texts = response.text_annotations
         text_string = ""
         for text in texts:
@@ -64,42 +53,55 @@ class VisionAPI:
 
 
 
+
 class TextProcessor:
     __author__ = "Yrian Hovde Øksne"
 
-    def get_invoice_date(text_string):
+    def __init__(self, text_string):
+        '''
+        The TextProcessor class serves as a parser of reciepts and invoices. The class need to be passed a text_string
+        made by the ImageProcessor class to function properly.
+        Note: class is used in syst.dev. subject to satisfy the owner of @Sukkertoppen's requirements.
+        :param text_string: a text string of the receipt or invoice you want to get information from. Needs to come from
+        the VisionManager class to work properly.
+        '''
+        self._text_string = text_string
+
+    def get_invoice_date(self):
         """
         gets invoice date from a text string made by the google vision api
         :param text_string: string of image processed data
         :return: invoice date
         """
 
-        match = re.search(r'\d{2}.\d{2}.\d{4}', text_string)
+        match = re.search(r'\d{2}.\d{2}.\d{4}', self._text_string)
         date = datetime.strptime(match.group(), '%d.%m.%Y').date()
-        print(date)
+        return date
 
-    def get_invoice_number(text_string):
+    def get_invoice_number(self):
         """
         gets invoice number from a larger string
         :param test_string: string that contains an invoice number
         :return: invoice number
         """
-    def get_total_amount_paid(text_string):
+    def get_total_amount_paid(self):
         """
         gets the total amount paid for within a string of a receipt processed by Google visions api
         :param text_string: processed string from the google vision api
         :return: total amount paid for in NOK
         """
-        start = text_string.index("NOK\n") +3;
-        end = text_string.index("GODKJENT\n")
-        match = text_string[start: end:1]
-        print(match)
+        start = self._text_string.index("NOK\n") +3;
+        end = self._text_string.index("GODKJENT\n")
+        match = self._text_string[start: end:1]
+        return match
         '''
         match2 = text_string.Substring(start, input.IndexOf("GODKJENT") - start);
         '''
 
-img_text = VisionAPI.get_text_detection_from_img("kvit2.jpg")
+vision_manager = VisionManager("key.json")
+img_text = vision_manager.get_text_detection_from_img("kvit2.jpg")
+text_processor = TextProcessor(img_text)
 print("Amount paid:")
-amount_paid = TextProcessor.get_total_amount_paid(img_text)
+print(text_processor.get_total_amount_paid())
 print("Invoice date:")
-invoice_date = TextProcessor.get_invoice_date(img_text)
+print(text_processor.get_invoice_date())

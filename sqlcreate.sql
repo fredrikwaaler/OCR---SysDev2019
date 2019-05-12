be2a71916f2cea20c36f73dbfb825a861b0ed1ba
fredrikwaaler <fredrik.waaler@gmail.com>
2019-05-12 18:54:19 +0200

    SQL

--- sqlcreate.sql
+++ sqlcreate.sql
@@ -1,15 +1,4 @@
 -- -----------------------------------------------------
--- Table for storing images sent from mobile app
--- -----------------------------------------------------
-DROP TABLE IF EXISTS File CASCADE;
-
-CREATE TABLE IF NOT EXISTS File (
-  file_id SERIAL PRIMARY KEY NOT NULL,
-  file bytea NOT NULL
-);
-
-
--- -----------------------------------------------------
 -- Table for storing the information about a user
 -- -----------------------------------------------------
 DROP TABLE IF EXISTS UserInfo;
